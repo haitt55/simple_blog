045c49b8aa65e36ed83379f311a89f6ad2c33bf3
haitt55 <haitt55@gmail.com>
2018-09-25 22:18:59 +0700

    fix login layout in admin and user

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 col-md-offset-2">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            {{ __('Login') }}
+                        </div>
+                    </div>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -39,27 +45,11 @@
                             </div>
                         </div>
 
-                        {{--<div class="form-group row">--}}
-                            {{--<div class="col-md-6 offset-md-4">--}}
-                                {{--<div class="form-check">--}}
-                                    {{--<input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>--}}
-
-                                    {{--<label class="form-check-label" for="remember">--}}
-                                        {{--{{ __('Remember Me') }}--}}
-                                    {{--</label>--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                        <div class="form-group row">
+                            <div class="col-md-8 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
-                                {{--<a class="btn btn-link" href="{{ route('password.request') }}">--}}
-                                    {{--{{ __('Forgot Your Password?') }}--}}
-                                {{--</a>--}}
                             </div>
                         </div>
                     </form>
